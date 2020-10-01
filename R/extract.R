270ead77eb03726d2926910ca929061f95e874fb
Anthony North <anthony.jl.north@gmail.com>
2020-10-01 18:38:29 +1000

    extract globals from a closure

---
+++ R/extract.R
@@ -0,0 +1,23 @@
+#' Extract globals
+#'
+#' @param func a function to extract from
+#' @keywords internal
+#' @noRd
+extract_globals <- function(func) {
+  env <- environment()
+  globals <- c()
+
+  enter <- function(type, name, expr, walker) {
+    undefined <- !exists(
+      name,
+      envir = walker$globalenv, mode = ifelse(type == "function", type, "any")
+    )
+
+    if (undefined && (type == "variable" || type == "function" && name == c(":="))) {
+      env$globals <- c(globals, name)
+    }
+  }
+
+  codetools::collectUsage(func, enterGlobal = enter)
+  unique(globals)
+}
