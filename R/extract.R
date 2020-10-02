52e4667fbd3d80ec013f91bf09d9334033bef39a
Anthony North <anthony.jl.north@gmail.com>
2020-10-02 23:45:15 +1000

    bugfix: names being matched in parent environments

--- R/extract.R
+++ R/extract.R
@@ -8,15 +8,25 @@ extract_globals <- function(func) {
   globals <- c()
 
   enter <- function(type, name, expr, walker) {
-    undefined <- !exists(
-      name, envir = walker$globalenv, mode = ifelse(type == "function", type, "any")
-    )
-
-    if (undefined && (type == "variable" || type == "function" && name == c(":="))) {
+    undefined <- !is_defined(name, walker$globalenv)
+    if (undefined && (type == "variable" || type == "function" && name == ":=")) {
       env$globals <- c(globals, name)
     }
   }
 
   codetools::collectUsage(func, enterGlobal = enter)
   unique(globals)
+}
+
+#' Is defined
+#'
+#' @param name a binding name
+#' @param env an environment
+#' @param inherit look for names in parent environments
+#'
+#' @keywords internal
+#' @noRd
+is_defined <- function(name, env, inherit = FALSE) {
+  exists(name, envir = env, inherits = inherit) ||
+    exists(name, envir = baseenv(), inherits = inherit)
 }
